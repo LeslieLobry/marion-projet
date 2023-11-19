feb74b28811f9b601fe9e8a62742e8c37b82ae7b
LeslieLobry <leslielobry@gmail.com>
2023-11-19 17:53:31 +0100

    push

--- src/Components/Accordeon/accordeon.jsx
+++ src/Components/Accordeon/accordeon.jsx
@@ -6,25 +6,22 @@ import chevron from "../../Assets/chevron.png"
 
 function Accordeon ({title, content}){
     const [ open, close] = useState(false);
-    // if (content=== null){
-    //     <div className=""
-    // }
-   
-    return (
-        <div className="accordion-container">
-            <div className="accordion"onClick={
-            ()=>close(!open)
-            }>
-            <div className="accordion-title" >{title}
-            </div>
-            <div className="chevron">
-            {close &&<img className={open ? 'chevron chevron_up':'chevron chevron_down'} src={chevron} alt="kasa"></img>} 
-            </div>
-            </div>
-            <div className="accordion-content">
-               {open && <p>{content}</p>}
+        return (
+            <div className="accordion-container">
+                <div className="accordion"onClick={
+                    ()=>close(!open)
+                }>
+                <div className="accordion-title" >{title}
+                </div>
+                <div className="chevron">
+                    {close &&<img className={open ? 'chevron chevron_up':'chevron chevron_down'} src={chevron} alt="kasa"></img>
+                    } 
+                </div>
+                </div>
+                <div className="accordion-content">
+                    {open && <p>{content}</p>}
             </div>
         </div>
     )
- }
+   }
  export default Accordeon
