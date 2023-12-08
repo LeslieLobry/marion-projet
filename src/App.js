1e99b2620438844fb4d3012ad4a45fdf573f8698
LeslieLobry <leslielobry@gmail.com>
2023-12-08 19:23:13 +0100

    resa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/home";
 import Error from "./pages/Erreur/erreur";
