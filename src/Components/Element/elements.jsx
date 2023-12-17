164b7de9e6ee7a5a1666c5f5f506939f8e66b781
LeslieLobry <leslielobry@gmail.com>
2023-12-17 18:15:07 +0100

    responsive +/-

--- src/Components/Element/elements.jsx
+++ src/Components/Element/elements.jsx
@@ -8,17 +8,17 @@ const { id } = useParams();
 const product = prestation.find((item) => item.id === id)
 const presta = product?.presta.map((presta, list)=>
 {
- return <li key={list}>{presta}</li>
-    })
-    const prix = product?.prix.map((prix, list)=>
-    {
-     return <li key={list}>{prix}</li>
-        })
+    return <li key={list}>{presta}</li>
+})
+const prix = product?.prix.map((prix, list)=>
+{
+    return <li key={list}>{prix}</li>
+})
     return(
             <div className="contenant">
                 <div className="titre">{presta}</div>
-                <div className="prix">{prix}</div>
-            </div>            
+            <div className="prix">{prix}</div>
+            </div>
 )
 }
 export default Presta
