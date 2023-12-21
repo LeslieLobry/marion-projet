b26497cecdec224b1c442bbcc75f9b7944ab7b36
LeslieLobry <leslielobry@gmail.com>
2023-12-21 09:53:06 +0100

    responsive

--- src/Components/Element/elements.jsx
+++ src/Components/Element/elements.jsx
@@ -6,17 +6,22 @@ import "../Element/elements.css"
 function Presta (){
 const { id } = useParams();
 const product = prestation.find((item) => item.id === id)
-const presta = product?.presta.map((presta, list)=>
+const presta = product?.presta.map((presta,list)=>
 {
  return <li key={list}>{presta}</li>
     })
-    const prix = product?.prix.map((prix, list)=>
+const prix = product?.prix.map((prix, list)=>
     {
      return <li key={list}>{prix}</li>
         })
+const details = product?.details.map((details,list)=>
+{
+ return <li key={list}>{details}</li>
+    })
     return(
             <div className="contenant">
                 <div className="titre">{presta}</div>
+                <div className="details">{details}</div>
                 <div className="prix">{prix}</div>
             </div>            
 )
